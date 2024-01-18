da470eb8f8b53e1866c24c0ef9bbe8cc62c6e398
Jon Leibham <jleibham@gmail.com>
2024-01-17 19:26:39 -0600

    init: start of mono-repo

---
+++ packages/design-system/vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import { resolve } from "path";
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "@bham/design-system",
+      fileName: "index.ts",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})

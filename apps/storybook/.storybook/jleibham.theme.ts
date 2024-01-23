49bb7cb7a7eefd37fe99302ab9963bdb37f3544f
Jon Leibham <jleibham@gmail.com>
2024-01-23 11:24:52 -0600

    feat: updates how count is set on data

---
+++ apps/storybook/.storybook/jleibham.theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Jon Leibham Portfolio Design System',
+  brandUrl: 'https://jonleibham.com',
+  brandTarget: '_blank',
+});
